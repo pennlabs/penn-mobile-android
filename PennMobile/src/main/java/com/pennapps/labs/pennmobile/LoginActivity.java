285baf5752a24bae7729cfd375fc374b889478ec
vivian <vivianhhuang@gmail.com>
2014-01-22 23:34:19 -0500

    initial commit

---
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.pennapps.labs.pennmobile;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.login, menu);
+        return true;
+    }
+    
+}
