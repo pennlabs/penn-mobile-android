7d4a8f599a6561fd30738b9fdf8f62fd01230edb
vivian <vivianhhuang@gmail.com>
2014-01-23 00:17:11 -0500

    added course activity, starting layout

---
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/CourseActivity.java
@@ -0,0 +1,12 @@
+package com.pennapps.labs.pennmobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class CourseActivity extends ActionBarActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course);
+    }
+}
--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/LoginActivity.java
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/LoginActivity.java
@@ -12,12 +12,13 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.activity_login);
     }
 
-
+    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.login, menu);
         return true;
     }
+    */
     
 }
