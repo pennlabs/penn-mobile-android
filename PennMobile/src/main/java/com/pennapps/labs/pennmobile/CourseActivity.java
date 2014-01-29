206e7190478681d9baa678c548c89f1a68fb3e73
vivian <vivianhhuang@gmail.com>
2014-01-29 17:41:28 -0500

    activity feeds data into xml

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/CourseActivity.java
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/CourseActivity.java
@@ -3,10 +3,37 @@ package com.pennapps.labs.pennmobile;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class CourseActivity extends ActionBarActivity {
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course);
+        setCourseInfo();
+    }
+
+    private void setCourseInfo() {
+        // hard coded for testing
+        String mCode = "CIS 110";
+        String mName = "Intro to Comp Prog";
+        String mAvgCourse = "2.7";
+        String mAvgInstr = "2.5";
+        String mAvgDiff = "2.8";
+
+        TextView codeTextView = (TextView) findViewById(R.id.course_code);
+        codeTextView.setText(mCode);
+        TextView nameTextView = (TextView) findViewById(R.id.course_name);
+        nameTextView.setText(mName);
+        TextView avgCourseTextView = (TextView) findViewById(R.id.course_avg_course);
+        avgCourseTextView.setText(mAvgCourse);
+        TextView avgInstrTextView = (TextView) findViewById(R.id.course_avg_instr);
+        avgInstrTextView.setText(mAvgInstr);
+        TextView avgDiffTextView = (TextView) findViewById(R.id.course_avg_diff);
+        avgDiffTextView.setText(mAvgDiff);
+
+
     }
 }
