6421f7fa54fa8e77d1919de6f7d58bf834a8d428
vivian <vivianhhuang@gmail.com>
2014-03-27 17:13:48 -0400

    adding directory search class, new main access point class

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/DirectorySearchActivity.java
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/DirectorySearchActivity.java
@@ -4,21 +4,19 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class LoginActivity extends Activity {
+public class DirectorySearchActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_directory_search);
     }
 
-    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.login, menu);
+        getMenuInflater().inflate(R.menu.directory_search, menu);
         return true;
     }
-    */
     
 }
---
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/MainActivity.java
@@ -0,0 +1,34 @@
+package com.pennapps.labs.pennmobile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public void startDirectory(View v) {
+        Intent intent = new Intent(this, DirectorySearchActivity.class);
+        startActivity(intent);
+    }
+
+    public void startCourseSearch(View v) {
+        Intent intent = new Intent(this, RegistrarSearchActivity.class);
+        startActivity(intent);
+    }
+}
