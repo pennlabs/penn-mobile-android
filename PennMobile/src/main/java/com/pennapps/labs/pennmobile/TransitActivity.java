f37e06437a464c80f0e646ac66db850f622e1bc1
vivian <vivianhhuang@gmail.com>
2014-04-24 17:21:44 -0400

    INPROGRESS

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/TransitActivity.java
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/TransitActivity.java
@@ -1,15 +1,28 @@
 package com.pennapps.labs.pennmobile;
 
 import android.content.Intent;
+import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.Menu;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class TransitActivity extends Activity {
+
+    private TransitAPI mAPI;
+    private ArrayList<BusStop> mTransitArr;
+    private double mLatitude;
+    private double mLongitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +38,13 @@ public class TransitActivity extends Activity {
             // TODO: sigh
         }
 
-        Log.v("vivlabs", service.getLastKnownLocation(LocationManager.GPS_PROVIDER).toString());
+        mAPI = new TransitAPI();
+        mAPI.setUrlPath("transit/stopinventory");
+
+        // Log.v("vivlabs", service.getLastKnownLocation(LocationManager.GPS_PROVIDER).toString());
+        Location mLocation = service.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        mLatitude = mLocation.getLatitude();
+        mLongitude = mLocation.getLongitude();
     }
 
 
@@ -34,6 +53,54 @@ public class TransitActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.transit, menu);
         return true;
+    }
+
+    private class GetRequestTask extends AsyncTask<Void, Void, Boolean> {
+
+        private JSONArray responseArr;
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            try {
+                JSONObject resultObj = mAPI.getCourse("");
+                responseArr = (JSONArray) resultObj.get("result_data");
+                if (responseArr.length() == 0) return false;
+                return true;
+            } catch (JSONException e) {
+                return false;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean valid) {
+            if (!valid) {
+                return;
+            }
+
+            try {
+                mTransitArr = new ArrayList<BusStop>();
+                JSONObject resp;
+
+                for (int i = 0; i < responseArr.length(); i++) {
+                    resp = (JSONObject) responseArr.get(i);
+                    BusStop stop = new BusStop(resp.get("BusStopId").toString(),
+                                               resp.get("BusStopName").toString(),
+                                               resp.get("Latitude").toString(),
+                                               resp.get("Longitude").toString());
+                    mTransitArr.add(stop);
+                }
+
+                ArrayList<String> sortedArr = new ArrayList<String>();
+                for (int i = 0; i < mTransitArr.size(); i++) {
+                    BusStop currentStop = mTransitArr.get(i);
+                    for (int j = 0; j < mTransitArr.size(); j++) {
+                        
+                    }
+                }
+            } catch (JSONException e) {
+
+            }
+        }
     }
 
 }
