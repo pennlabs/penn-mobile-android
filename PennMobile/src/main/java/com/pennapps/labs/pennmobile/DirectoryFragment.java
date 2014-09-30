72d975bd37527a4d30af3bc3167e72ad9fba34f6
David Lakata <dlakata@seas.upenn.edu>
2014-09-30 19:40:50 -0400

    Moved directory search to action bar

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/DirectoryFragment.java
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/DirectoryFragment.java
@@ -1,13 +1,21 @@
 package com.pennapps.labs.pennmobile;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 import com.pennapps.labs.pennmobile.adapters.DirectoryAdapter;
 import com.pennapps.labs.pennmobile.api.DirectoryAPI;
@@ -27,6 +35,10 @@ public class DirectoryFragment extends ListFragment {
     private Context mContext;
     private String mFirstName;
     private String mLastName;
+    public static final String FIRST_NAME_INTENT_EXTRA = "FIRST_NAME";
+    public static final String LAST_NAME_INTENT_EXTRA = "LAST_NAME";
+    private SearchView searchView;
+    private TextView textView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -48,7 +60,62 @@ public class DirectoryFragment extends ListFragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        setHasOptionsMenu(true);
         mListView = getListView();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.directory_search:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.directory, menu);
+
+        searchView = (SearchView) menu.findItem(R.id.directory_search).getActionView();
+        final SearchView.OnQueryTextListener queryListener = new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextChange(String arg0) {
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextSubmit(String arg0) {
+                // TODO: error check for filled in fields
+                Fragment fragment = new DirectoryFragment();
+                Bundle args = new Bundle();
+                String[] query = arg0.split("\\s+");
+                if (query.length == 0) {
+                    args.putString(FIRST_NAME_INTENT_EXTRA, "");
+                    args.putString(FIRST_NAME_INTENT_EXTRA, "");
+                } else if (query.length == 1) {
+                    args.putString(FIRST_NAME_INTENT_EXTRA, query[0].replaceAll("\\s+",""));
+                    args.putString(LAST_NAME_INTENT_EXTRA, "");
+                } else {
+                    args.putString(FIRST_NAME_INTENT_EXTRA, query[0].replaceAll("\\s+",""));
+                    args.putString(LAST_NAME_INTENT_EXTRA, query[1].replaceAll("\\s+",""));
+                }
+                fragment.setArguments(args);
+
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.content_frame, fragment)
+                        .commit();
+                return true;
+            }
+        };
+        int id = searchView.getContext().getResources().getIdentifier("android:id/search_src_text", null, null);
+        textView = (TextView) searchView.findViewById(id);
+        textView.setTextColor(Color.WHITE);
+        searchView.setOnQueryTextListener(queryListener);
     }
 
     private class GetRequestTask extends AsyncTask<Void, Void, Boolean> {
@@ -56,7 +123,13 @@ public class DirectoryFragment extends ListFragment {
         private JSONArray responseArr;
 
         GetRequestTask(String firstName, String lastName) {
-            urlParameter = "first_name=" + firstName + "&last_name=" + lastName;
+            if (firstName.equals("") && lastName.equals("")) {
+                urlParameter = "";
+            } else if (lastName.equals("")) {
+                urlParameter = "first_name=" + firstName;
+            } else {
+                urlParameter = "first_name=" + firstName + "&last_name=" + lastName;
+            }
         }
 
         @Override
--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/DirectorySearchFragment.java
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/DirectorySearchFragment.java
@@ -1,44 +1,95 @@
 package com.pennapps.labs.pennmobile;
 
+import android.database.Cursor;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SearchView;
+import android.widget.TextView;
 
-public class DirectorySearchFragment extends Fragment implements View.OnClickListener {
+import com.pennapps.labs.pennmobile.adapters.RegistrarAdapter;
 
-    private EditText mFirstName;
-    private EditText mLastName;
+public class DirectorySearchFragment extends Fragment {
+
     public static final String FIRST_NAME_INTENT_EXTRA = "FIRST_NAME";
     public static final String LAST_NAME_INTENT_EXTRA = "LAST_NAME";
+    private SearchView searchView;
+    private TextView textView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_directory_search, container, false);
-        mFirstName = (EditText) v.findViewById(R.id.directory_first_name);
-        mLastName = (EditText) v.findViewById(R.id.directory_last_name);
-        Button b = (Button) v.findViewById(R.id.directory_search_button);
-        b.setOnClickListener(this);
+        View v = inflater.inflate(R.layout.fragment_directory, container, false);
         return v;
     }
 
     @Override
-    public void onClick(View view) {
-        // TODO: error check for filled in fields
-        Fragment fragment = new DirectoryFragment();
-        Bundle args = new Bundle();
-        args.putString(FIRST_NAME_INTENT_EXTRA, mFirstName.getText().toString());
-        args.putString(LAST_NAME_INTENT_EXTRA, mLastName.getText().toString());
-        fragment.setArguments(args);
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
-        FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.content_frame, fragment)
-                .commit();
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.directory_search:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.directory, menu);
+
+        searchView = (SearchView) menu.findItem(R.id.directory_search).getActionView();
+        final SearchView.OnQueryTextListener queryListener = new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextChange(String arg0) {
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextSubmit(String arg0) {
+                // TODO: error check for filled in fields
+                Fragment fragment = new DirectoryFragment();
+                Bundle args = new Bundle();
+                String[] query = arg0.split("\\s+");
+                if (query.length == 0) {
+                    args.putString(FIRST_NAME_INTENT_EXTRA, "");
+                    args.putString(FIRST_NAME_INTENT_EXTRA, "");
+                } else if (query.length == 1) {
+                    args.putString(FIRST_NAME_INTENT_EXTRA, query[0].replaceAll("\\s+",""));
+                    args.putString(LAST_NAME_INTENT_EXTRA, "");
+                } else {
+                    args.putString(FIRST_NAME_INTENT_EXTRA, query[0].replaceAll("\\s+",""));
+                    args.putString(LAST_NAME_INTENT_EXTRA, query[1].replaceAll("\\s+",""));
+                }
+                fragment.setArguments(args);
+
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.content_frame, fragment)
+                        .commit();
+                return true;
+            }
+        };
+        int id = searchView.getContext().getResources().getIdentifier("android:id/search_src_text", null, null);
+        textView = (TextView) searchView.findViewById(id);
+        textView.setTextColor(Color.WHITE);
+        searchView.setOnQueryTextListener(queryListener);
     }
 
 }
