14ce9172838219753e4a60658ef568e3a934627d
Adel Qalieh <aqalieh95@gmail.com>
2014-11-16 21:38:21 -0500

    Remove unnecessary imports

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/MainActivity.java
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/MainActivity.java
@@ -3,23 +3,18 @@ package com.pennapps.labs.pennmobile;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.content.res.Configuration;
-import android.widget.TextView;
 import com.crashlytics.android.Crashlytics;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class MainActivity extends ActionBarActivity {
 
