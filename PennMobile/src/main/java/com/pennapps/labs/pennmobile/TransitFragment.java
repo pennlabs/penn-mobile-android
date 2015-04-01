26c6baec3a16ef4dc6d8041f1dd749b19acb94e8
David Lakata <dlakata@seas.upenn.edu>
2015-03-31 20:41:13 -0400

    Transit is now a google map

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/TransitFragment.java
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/TransitFragment.java
@@ -1,41 +1,29 @@
 package com.pennapps.labs.pennmobile;
 
 import android.content.Context;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.LatLng;
 
 public class TransitFragment extends Fragment {
 
-    WebView mWebView;
-    private boolean mIsWebViewAvailable;
-    private View mView;
-    private RelativeLayout Pbar;
-
-    public TransitFragment() {
-        super();
-    }
+    private MapView mapView;
+    private GoogleMap googleMap;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        mWebView = new WebView(getActivity());
-        getActivity().getWindow().setFlags(
-                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
-                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
 
         InputMethodManager inputManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
 
@@ -47,48 +35,32 @@ public class TransitFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        mView = inflater.inflate(R.layout.fragment_transit, container, false);
+        View v = inflater.inflate(R.layout.fragment_transit, container, false);
 
-        Pbar = (RelativeLayout) mView.findViewById(R.id.loadingPanel);
 
-        mIsWebViewAvailable = true;
-        mWebView = (WebView) mView.findViewById(R.id.transitWebView);
-        mWebView.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
-                    mWebView.goBack();
-                    return true;
-                }
-                return false;
-            }
-        });
-        WebSettings webSettings = mWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        mWebView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
-        mWebView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-        mWebView.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public void onProgressChanged(WebView view, int progress)
-            {
-                if(progress >= 90) {
-                    ViewGroup parentWebView = (ViewGroup) mWebView.getParent();
-                    if (parentWebView != null) {
-                        parentWebView.removeView(mWebView);
-                    }
-                    Pbar.setVisibility(View.GONE);
-                    ((LinearLayout) mView).addView(mWebView);
-                }
-            }
-        });
-        mWebView.setWebViewClient(new InnerWebViewClient()); // forces it to open in app
-        String mUrl = "http://www.pennrides.com/map?showHeader=0&route=229,230&silent_disable_timeout=1";
-        mWebView.loadUrl(mUrl);
-        return mView;
-    }
+        mapView = (MapView) v.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
 
-    /* To ensure links open within the application */
-    private class InnerWebViewClient extends WebViewClient {
+        googleMap = mapView.getMap();
+        googleMap.getUiSettings().setMyLocationButtonEnabled(false);
+        googleMap.setMyLocationEnabled(true);
+
+        try {
+            MapsInitializer.initialize(this.getActivity());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Location location = googleMap.getMyLocation();
+        LatLng myLocation = new LatLng(39.9529, -75.197098);
+
+        if (location != null) {
+            myLocation = new LatLng(location.getLatitude(),
+                    location.getLongitude());
+        }
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 14));
+
+
+        return v;
     }
 
     @Override
@@ -102,50 +74,21 @@ public class TransitFragment extends Fragment {
         setRetainInstance(true);
     }
 
-    /**
-     * Called when the fragment is visible to the user and actively running. Resumes the WebView.
-     */
-    @Override
-    public void onPause() {
-        super.onPause();
-        mWebView.onPause();
-    }
-
-    /**
-     * Called when the fragment is no longer resumed. Pauses the WebView.
-     */
     @Override
     public void onResume() {
-        mWebView.onResume();
+        mapView.onResume();
         super.onResume();
     }
 
-    /**
-     * Called when the WebView has been detached from the fragment.
-     * The WebView is no longer available after this time.
-     */
-    @Override
-    public void onDestroyView() {
-        mIsWebViewAvailable = false;
-        super.onDestroyView();
-    }
-
-    /**
-     * Called when the fragment is no longer in use. Destroys the internal state of the WebView.
-     */
     @Override
     public void onDestroy() {
-        if (mWebView != null) {
-            mWebView.destroy();
-            mWebView = null;
-        }
         super.onDestroy();
+        mapView.onDestroy();
     }
 
-    /**
-     * Gets the WebView.
-     */
-    public WebView getWebView() {
-        return mIsWebViewAvailable ? mWebView : null;
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
