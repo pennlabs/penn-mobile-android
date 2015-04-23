0265a29a2d8779f68b0dd1887c612eaa10c22ee3
Jason <tangsh@sas.upenn.edu>
2015-04-22 20:54:48 -0400

    Implement building api into transit fragment

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/TransitFragment.java
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/TransitFragment.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
-import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.SearchView;
@@ -28,19 +27,16 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.pennapps.labs.pennmobile.api.Labs;
 import com.pennapps.labs.pennmobile.classes.Building;
-import com.pennapps.labs.pennmobile.classes.BusRoute;
+import com.pennapps.labs.pennmobile.classes.BusPath;
 import com.pennapps.labs.pennmobile.classes.BusStop;
 
 import java.io.IOException;
-import java.util.LinkedList;
 import java.util.List;
 
 import rx.android.schedulers.AndroidSchedulers;
@@ -85,13 +81,13 @@ public class TransitFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_transit, container, false);
 
-
         mapView = (MapView) v.findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
 
         googleMap = mapView.getMap();
         googleMap.getUiSettings().setMyLocationButtonEnabled(false);
         googleMap.setMyLocationEnabled(true);
+        googleMap.setInfoWindowAdapter(new CustomWindowAdapter(inflater));
 
         try {
             MapsInitializer.initialize(this.getActivity());
@@ -106,10 +102,7 @@ public class TransitFragment extends Fragment {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if((actionId == EditorInfo.IME_ACTION_DONE || event != null) && query != null && !query.isEmpty()){
-                    LatLng start = getLatLng(v.getEditableText().toString());
-                    if(start != null){
-                        searchTransit(query, start);
-                    }
+                    drawMap(query, v.getEditableText().toString(), null);
                 }
                 return false;
             }
@@ -170,6 +163,7 @@ public class TransitFragment extends Fragment {
             public boolean onQueryTextChange(String arg0) {
                 if(arg0.isEmpty()){
                     startingLoc.setVisibility(View.GONE);
+                    startingLoc.setText("");
                 }
                 return true;
             }
@@ -177,7 +171,7 @@ public class TransitFragment extends Fragment {
             @Override
             public boolean onQueryTextSubmit(String arg0) {
                 query = arg0;
-                searchTransit(query);
+                drawMap(query, null, mapCallBacks.getLatLng());
                 startingLoc.setVisibility(View.VISIBLE);
                 return true;
             }
@@ -185,12 +179,20 @@ public class TransitFragment extends Fragment {
         searchView.setOnQueryTextListener(queryListener);
     }
 
-    public LatLng getLatLng(String destination) {
-        LatLng start = mapCallBacks.getLatLng();
+    public void drawMap(String destination, String start, LatLng current) {
+        final String begin = start;
+        final LatLng beginL = current;
         final String dest = destination;
-        //first try building api
-        final LinkedList<LatLng> bufflist = new LinkedList<LatLng>();
-        mLabs.buildings(query)
+        View view = getActivity().getCurrentFocus();
+        if (view != null) {
+            InputMethodManager inputManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputManager.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+        String q = destination;
+        if(current == null){
+            q = start;
+        }
+        mLabs.buildings(q)
                 .observeOn(AndroidSchedulers.mainThread()).onErrorReturn(new Func1<Throwable, List<Building>>() {
             @Override
             public List<Building> call(Throwable throwable) {
@@ -199,10 +201,12 @@ public class TransitFragment extends Fragment {
         }).subscribe(new Action1<List<Building>>() {
             @Override
             public void call(List<Building> buildings) {
+                LatLng latLng = null;
                 if (!buildings.isEmpty()) {
-
-                    bufflist.add(new LatLng (Double.parseDouble(buildings.get(0).latitude),
-                            Double.parseDouble(buildings.get(0).longitude)));
+                    latLng = new LatLng(Double.parseDouble(buildings.get(0).latitude),
+                            Double.parseDouble(buildings.get(0).longitude));
+                    Toast.makeText(getActivity().getApplicationContext(),
+                            "latLng = " + latLng, Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(getActivity().getApplicationContext(), "No results found.",
                             Toast.LENGTH_LONG).show();
@@ -210,10 +214,9 @@ public class TransitFragment extends Fragment {
                     try {
                         List<Address> locationList = geocoder.getFromLocationName(dest, 1);
                         if (locationList.size() > 0) {
-                            bufflist.add(new LatLng(locationList.get(0).getLatitude(),
-                                    locationList.get(0).getLongitude()));
-                        }
-                        else{
+                            latLng = new LatLng(locationList.get(0).getLatitude(),
+                                    locationList.get(0).getLongitude());
+                        } else {
                             Toast.makeText(getActivity().getApplicationContext(),
                                     "Location not found, please try again", Toast.LENGTH_SHORT).show();
                             searchView.setQuery("", false);
@@ -222,71 +225,80 @@ public class TransitFragment extends Fragment {
                         e.printStackTrace();
                     }
                 }
+                if(beginL == null){
+                    drawMap(dest, begin, latLng);
+                }
+                else {
+                    final LatLng endL = latLng;
+                    Toast.makeText(getActivity().getApplicationContext(), latLng.toString(), Toast.LENGTH_SHORT).show();
+                    mLabs.routing(String.valueOf(beginL.latitude), Double.toString(endL.latitude),
+                            String.valueOf(beginL.longitude), Double.toString(endL.longitude))
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .onErrorReturn(new Func1<Throwable, BusPath>() {
+                                @Override
+                                public BusPath call(Throwable throwable) {
+                                    return new BusPath();
+                                }
+                            })
+                            .subscribe(new Action1<BusPath>() {
+                                @Override
+                                public void call(BusPath route) {
+                                    googleMap.clear();
+                                    if (route == null) {
+                                        Toast.makeText(getActivity().getApplicationContext(), "No path found.", Toast.LENGTH_SHORT).show();
+                                        return;
+                                    }
+                                    Toast.makeText(getActivity().getApplicationContext(),
+                                            "route found", Toast.LENGTH_SHORT).show();
+                                    PolylineOptions options = new PolylineOptions();
+                                    for (BusStop busStop : route.path) {
+                                        Toast.makeText(getActivity().getApplicationContext(),
+                                                busStop.getLatitude() + " " + busStop.getLongitude(), Toast.LENGTH_SHORT).show();
+                                        LatLng latLngBuff = new LatLng(busStop.getLatitude(), busStop.getLongitude());
+                                        googleMap.addMarker(new MarkerOptions()
+                                                        .position(latLngBuff)
+                                                        .title(busStop.getName()));
+                                        options.add(latLngBuff);
+                                    }
+                                    options.width(15).color(Color.BLUE);
+                                    googleMap.addPolyline(options);
+                                    PolylineOptions startwalk = new PolylineOptions();
+                                    startwalk.add(beginL);
+                                    startwalk.add(new LatLng(route.path.get(0).getLatitude(), route.path.get(0).getLongitude()));
+                                    startwalk.color(Color.RED).width(15);
+                                    googleMap.addPolyline(startwalk);
+                                    PolylineOptions endwalk = new PolylineOptions();
+                                    endwalk.add(new LatLng(route.path.get(route.path.size() - 1).getLatitude(), route.path.get(route.path.size() - 1).getLongitude()));
+                                    endwalk.add(endL);
+                                    endwalk.color(Color.RED).width(15);
+                                    googleMap.addPolyline(endwalk);
+                                }
+                            });
+                }
             }
         });
-        if(bufflist.size() == 1){
-            Toast.makeText(getActivity().getApplicationContext(), "found it",
-                    Toast.LENGTH_LONG).show();
-            return bufflist.get(0);
-        }
-        return null;
     }
 
-    private void searchTransit(String query) {
-        searchTransit(query, mapCallBacks.getLatLng());
-    }
+    private class CustomWindowAdapter implements GoogleMap.InfoWindowAdapter {
 
-    private void searchTransit(String query, LatLng start){
-        final LatLng startLatLng = start;
-        final LatLng latLng = getLatLng(query);
-        if(latLng == null){
-            return;
+        private View view;
+        LayoutInflater inflater = null;
+
+        public CustomWindowAdapter(LayoutInflater inflater) {
+            this.inflater = inflater;
+            view = inflater.inflate(R.layout.info_window, null);
         }
-        View view = getActivity().getCurrentFocus();
-        if (view != null) {
-            InputMethodManager inputManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-            inputManager.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+        @Override
+        public View getInfoWindow(Marker arg0) {
+            return null;
         }
-        Toast.makeText(getActivity().getApplicationContext(), latLng.toString(), Toast.LENGTH_SHORT).show();
-        mLabs.routing(String.valueOf(start.latitude), Double.toString(latLng.latitude),
-                String.valueOf(start.longitude), Double.toString(latLng.longitude))
-                .observeOn(AndroidSchedulers.mainThread())
-                .onErrorReturn(new Func1<Throwable, BusRoute>() {
-                    @Override
-                    public BusRoute call(Throwable throwable) {
-                        return new BusRoute();
-                    }
-                })
-                .subscribe(new Action1<BusRoute>() {
-                    @Override
-                    public void call(BusRoute route) {
-                        googleMap.clear();
-                        if(route == null){
-                            Toast.makeText(getActivity().getApplicationContext(), "No path found.", Toast.LENGTH_SHORT).show();
-                            return;
-                        }
-                        PolylineOptions options = new PolylineOptions();
-                        for (BusStop busStop : route.path) {
-                            LatLng latLngBuff = new LatLng(busStop.getLatitude(), busStop.getLongitude());
-                            googleMap.addCircle(new CircleOptions()
-                                    .center(latLngBuff)
-                                    .radius(10))
-                                    .setFillColor(Color.BLACK);
-                            options.add(latLngBuff);
-                        }
-                        options.width(15).color(Color.BLUE);
-                        googleMap.addPolyline(options);
-                        PolylineOptions startwalk = new PolylineOptions();
-                        startwalk.add(startLatLng);
-                        startwalk.add(new LatLng(route.path.get(0).getLatitude(), route.path.get(0).getLongitude()));
-                        startwalk.color(Color.RED).width(15);
-                        googleMap.addPolyline(startwalk);
-                        PolylineOptions endwalk = new PolylineOptions();
-                        endwalk.add(new LatLng(route.path.get(route.path.size()-1).getLatitude(), route.path.get(route.path.size()-1).getLongitude()));
-                        endwalk.add(latLng);
-                        endwalk.color(Color.RED).width(15);
-                        googleMap.addPolyline(endwalk);
-                    }
-                });
+
+        @Override
+        public View getInfoContents(final Marker arg0) {
+            TextView name = (TextView) view.findViewById(R.id.building_name);
+            name.setText(arg0.getTitle());
+            return view;
+        }
     }
 }
