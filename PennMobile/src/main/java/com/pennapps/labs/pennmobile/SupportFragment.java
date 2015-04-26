8f3ab9a8287d3a0a0dd07d75072405ac624f1866
Adel Qalieh <aqalieh95@gmail.com>
2015-04-26 17:01:21 -0400

    Add new support numbers
    Fixes #155

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/SupportFragment.java
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/SupportFragment.java
@@ -34,11 +34,15 @@ public class SupportFragment extends ListFragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         mListView = getListView();
         List<Person> contacts_list = new ArrayList<>();
-        contacts_list.add(new Person("Penn Police General", "215 898-7297"));
-        contacts_list.add(new Person("Penn Police Emergencies/MERT", "215 573-3333"));
+        contacts_list.add(new Person("Penn Police General", "(215) 898-7297"));
+        contacts_list.add(new Person("Penn Police Emergencies/MERT", "(215) 573-3333"));
         contacts_list.add(new Person("Penn Walk", "215-898-9255", "215-898-WALK"));
         contacts_list.add(new Person("Penn Ride", "215-898-7433", "215-898-RIDE"));
         contacts_list.add(new Person("Help Line", "215-898-4357", "215-898-HELP"));
+        contacts_list.add(new Person("CAPS", "(215) 898-7021"));
+        contacts_list.add(new Person("Special Services", "(215) 898-6600"));
+        contacts_list.add(new Person("Women's Center", "(215) 898-8611"));
+        contacts_list.add(new Person("Student Health Services", "(215) 746-3535"));
 
         SupportAdapter supportAdapter = new SupportAdapter(getActivity().getApplicationContext(), contacts_list);
 
