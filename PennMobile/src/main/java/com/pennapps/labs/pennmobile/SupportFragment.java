822c1f47c492a57e304e2f63c432e3a722c5617e
Adel Qalieh <aqalieh95@gmail.com>
2015-04-26 18:53:25 -0400

    Use ViewHolder for Support Fragment

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/SupportFragment.java
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/SupportFragment.java
@@ -1,30 +1,20 @@
 package com.pennapps.labs.pennmobile;
 
 
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
+import com.pennapps.labs.pennmobile.adapters.SupportAdapter;
 import com.pennapps.labs.pennmobile.classes.Person;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
-/**
- * A simple {@link ListFragment} subclass.
- */
 public class SupportFragment extends ListFragment {
-
-    private ListView mListView;
 
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +22,7 @@ public class SupportFragment extends ListFragment {
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
-        mListView = getListView();
+        ListView mListView = getListView();
         List<Person> contacts_list = new ArrayList<>();
         contacts_list.add(new Person("Penn Police General", "(215) 898-7297"));
         contacts_list.add(new Person("Penn Police Emergencies/MERT", "(215) 573-3333"));
@@ -55,46 +45,4 @@ public class SupportFragment extends ListFragment {
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_support, container, false);
     }
-
-    private class SupportAdapter extends ArrayAdapter<Person> {
-
-        public SupportAdapter(Context context, List<Person> contacts) {
-            super(context, R.layout.support_list_item, contacts);
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            final Person currentPerson = getItem(position);
-
-            View view = LayoutInflater.from(getContext())
-                    .inflate(R.layout.support_list_item, null);
-
-            TextView name = (TextView) view.findViewById(R.id.support_name);
-            TextView phone = (TextView) view.findViewById(R.id.support_phone);
-
-            name.setText(currentPerson.name);
-            if (currentPerson.phone_words.equals("")) {
-                phone.setText(currentPerson.phone);
-            } else {
-                phone.setText(currentPerson.phone_words + " (" + currentPerson.phone + ")");
-            }
-
-
-            view.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    String uri = "tel:" + currentPerson.phone;
-                    Intent intent = new Intent(Intent.ACTION_DIAL);
-                    intent.setData(Uri.parse(uri));
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    v.getContext().startActivity(intent);
-                }
-            });
-
-            return view;
-        }
-
-    }
-
-
 }
---
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/adapters/SupportAdapter.java
@@ -0,0 +1,69 @@
+package com.pennapps.labs.pennmobile.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.pennapps.labs.pennmobile.R;
+import com.pennapps.labs.pennmobile.classes.Person;
+
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class SupportAdapter extends ArrayAdapter<Person> {
+    private final LayoutInflater inflater;
+
+    public SupportAdapter(Context context, List<Person> contacts) {
+        super(context, R.layout.support_list_item, contacts);
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        final Person currentPerson = getItem(position);
+        ViewHolder holder;
+        if (view != null) {
+            holder = (ViewHolder) view.getTag();
+        } else {
+            view = inflater.inflate(R.layout.support_list_item, parent, false);
+            holder = new ViewHolder(view);
+            view.setTag(holder);
+        }
+
+        holder.name.setText(currentPerson.name);
+        if (currentPerson.phone_words.equals("")) {
+            holder.phone.setText(currentPerson.phone);
+        } else {
+            holder.phone.setText(currentPerson.phone_words + " (" + currentPerson.phone + ")");
+        }
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String uri = "tel:" + currentPerson.phone;
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse(uri));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                v.getContext().startActivity(intent);
+            }
+        });
+
+        return view;
+    }
+
+    public static class ViewHolder {
+        @InjectView(R.id.support_name) TextView name;
+        @InjectView(R.id.support_phone) TextView phone;
+
+        public ViewHolder(View view) {
+            ButterKnife.inject(this, view);
+        }
+    }
+}
