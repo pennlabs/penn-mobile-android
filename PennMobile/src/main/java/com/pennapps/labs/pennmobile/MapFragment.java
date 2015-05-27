52e672c1fa1705c29acc37f5e86fc6ab1d27d5bd
Adel Qalieh <aqalieh95@gmail.com>
2015-05-27 14:28:21 -0400

    Fix crash for ProgressBar color changes in <5.0

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/MapFragment.java
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/MapFragment.java
@@ -1,6 +1,5 @@
 package com.pennapps.labs.pennmobile;
 
-import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationProvider;
@@ -14,7 +13,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
