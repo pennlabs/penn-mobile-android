51da1251489c0fccb7eee350253198c5fb0eec93
Andrew Si <andrewsi10@gmail.com>
2018-02-23 18:12:16 -0500

    cleaned up imports and removed log statements

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/GsrFragment.java
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/GsrFragment.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -397,7 +396,6 @@ public class GsrFragment extends Fragment {
                             @Override
                             public void run() {
                                 Toast.makeText(getActivity(), "Error: Could not retrieve GSRs", Toast.LENGTH_LONG).show();
-                                Log.e("error", throwable.toString());
                             }
                         });
                     }
