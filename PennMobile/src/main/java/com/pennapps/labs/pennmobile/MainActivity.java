fabd5cb712298a7abb601b12d46c6145d60dadd9
Jackie Peng <jackiepeng12@gmail.com>
2018-03-14 17:43:30 -0400

    delete Log statements

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/MainActivity.java
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/MainActivity.java
@@ -23,7 +23,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -189,8 +188,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void navigateLayout(@AnyRes int id) {
-
-        Log.i("MainActivity", "navigateLayout");
 
         Fragment fragment = null;
         switch (id) {
