2a8ff794633f7e981eb21400bfd33ea2b09390d3
andrewsi10 <andrewsi10@gmail.com>
2018-04-11 20:14:29 -0400

    Removed log imports

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/BookGsrFragment.java
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/BookGsrFragment.java
@@ -3,7 +3,6 @@ package com.pennapps.labs.pennmobile;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/GsrRoomAdapter.java
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/GsrRoomAdapter.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
