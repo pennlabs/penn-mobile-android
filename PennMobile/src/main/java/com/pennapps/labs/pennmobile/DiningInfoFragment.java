f25e72154b1186142d3b3498ac3e2abfd7dbda11
andrewsi10 <andrewsi10@gmail.com>
2018-12-02 12:08:47 -0500

    Fixed bug where back button replaces hamburger icon

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/DiningInfoFragment.java
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/DiningInfoFragment.java
@@ -8,8 +8,6 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
@@ -184,8 +182,6 @@ public class DiningInfoFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onResume() {
         super.onResume();
-        mActivity.getActionBarToggle().setDrawerIndicatorEnabled(false);
-        mActivity.getActionBarToggle().syncState();
         getActivity().setTitle(mDiningHall.getName());
         if (map == null) {
             mapFragment.getMapAsync(this);
