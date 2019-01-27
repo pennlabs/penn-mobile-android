df76473be5cf8adc05ab904443838b545b5f727e
Skyler Rankin <skylerrankin@gmail.com>
2019-01-27 12:53:34 -0500

    change to kotlin, remove butterknife

---
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/FlingFragment.kt
@@ -0,0 +1,59 @@
+package com.pennapps.labs.pennmobile
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.answers.Answers
+import com.crashlytics.android.answers.ContentViewEvent
+import com.pennapps.labs.pennmobile.adapters.FlingRecyclerViewAdapter
+
+import io.fabric.sdk.android.Fabric
+import kotlinx.android.synthetic.main.fragment_fling.*
+
+class FlingFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Fabric.with(context, Crashlytics())
+        Answers.getInstance().logContentView(ContentViewEvent()
+                .putContentName("Spring Fling")
+                .putContentType("App Feature")
+                .putContentId("7"))
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_fling, container, false)
+        val labs = MainActivity.getLabsInstance()
+        labs.flingEvents.subscribe({ flingEvents ->
+            activity?.runOnUiThread {
+                fling_fragment_recyclerview.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+                fling_fragment_recyclerview.adapter = FlingRecyclerViewAdapter(context, flingEvents)
+            }
+        }, { activity?.runOnUiThread { Toast.makeText(activity, "Error: Could not retrieve Spring Fling schedule", Toast.LENGTH_LONG).show() } })
+        return view
+    }
+
+    override fun onResume() {
+        super.onResume()
+        activity?.setTitle(R.string.spring_fling)
+    }
+
+    override fun onAttach(context: Context?) { super.onAttach(context) }
+    override fun onDetach() { super.onDetach() }
+    override fun onDestroyView() { super.onDestroyView() }
+
+    companion object {
+        fun newInstance(): FlingFragment {
+            return FlingFragment()
+        }
+    }
+}// Required empty public constructor
