7dffc91ce1a0cb7ebdc16450bf5d6cc0ae5a3b3e
Andrew Si <andrewsi10@gmail.com>
2019-03-17 14:08:30 -0400

    fixed webview and cleaned up code

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/HuntsmanGSRLogin.kt
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/HuntsmanGSRLogin.kt
@@ -6,33 +6,32 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.book_gsr.*
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.fragment_huntsman_gsrlogin.*
 
-/**
- * A simple [Fragment] subclass.
- *
- */
-class huntsmanGSRLogin : Fragment() {
+class HuntsmanGSRLogin : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val v = inflater.inflate(R.layout.fragment_huntsman_gsrlogin, container, false)
-
-        loadWebpage()
-
         return v
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        loadWebpage()
     }
 
     private fun loadWebpage() {
         // Get the web view settings instance
+        webViewGSR.webViewClient = WebViewClient()
         webViewGSR.loadUrl("https://apps.wharton.upenn.edu/gsr/")
     }
 
     companion object {
 
-        fun newInstance(): huntsmanGSRLogin {
-            return huntsmanGSRLogin()
+        fun newInstance(): HuntsmanGSRLogin {
+            return HuntsmanGSRLogin()
         }
     }
 }
