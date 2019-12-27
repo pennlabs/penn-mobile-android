096ad0d582e24fafa95239822f2b32aa081ddce9
Marta <martagarciaferreiro@gmail.com>
2019-12-27 18:35:07 +0100

    added photos to the about page, changed app font to segoe ui

---
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/adapters/AboutAdapter.kt
@@ -0,0 +1,43 @@
+package com.pennapps.labs.pennmobile.adapters
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pennapps.labs.pennmobile.R
+import kotlinx.android.synthetic.main.team_member.view.*
+
+class AboutAdapter(private var members: ArrayList<String>)
+    : RecyclerView.Adapter<AboutAdapter.TeamViewHolder>() {
+
+    private lateinit var mContext: Context
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TeamViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.team_member, parent, false)
+        mContext = parent.context
+        return TeamViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return members.count()
+    }
+
+    override fun onBindViewHolder(holder: TeamViewHolder, position: Int) {
+        holder.itemView.name_tv.text = members[position]
+        var imageId = when (members[position]) {
+            "Marta García Ferreiro" -> R.drawable.marta
+            "Varun Ramakrishnan" -> R.drawable.varun
+            "Anna Wang" -> R.drawable.anna
+            "Davies Lumumba" -> R.drawable.davies
+            "Sophia Ye" -> R.drawable.sophia
+            "Sahit Penmatcha" -> R.drawable.sahit
+            else -> null
+        }
+        if (imageId != null) holder.itemView.person_iv.setImageResource(imageId)
+    }
+
+    inner class TeamViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val view = itemView
+    }
+}
