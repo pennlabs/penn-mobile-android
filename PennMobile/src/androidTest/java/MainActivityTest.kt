eb54b17d71562922e7f2d510bcbab66dbd636426
mgarciaferreiro <martagarciaferreiro@gmail.com>
2021-04-25 21:07:42 -0400

    builds with secrets in local.properties

--- PennMobile/src/androidTest/java/MainActivityTest.kt
+++ PennMobile/src/androidTest/java/MainActivityTest.kt
@@ -1,34 +1,34 @@
 package com.pennapps.labs.pennmobile
 
-import androidx.test.rule.ActivityTestRule
-import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
-import org.junit.Rule
-import org.junit.Test
-
-// Instrumentation tests, written using Kaspresso https://github.com/KasperskyLab/Kaspresso
-class MainActivityTest : TestCase() {
-
-    @get:Rule
-    val activityTestRule = ActivityTestRule(MainActivity::class.java, true, false)
-
-    @Test
-    fun testMainActivity() {
-        before {
-            activityTestRule.launchActivity(null)
-            testLogger.i("Starting test")
-        }.after {
-        }.run {
-            step("Test setup") {
-                MainScreen {
-                    toolbarTitle.hasText("Penn Mobile")
-                }
-            }
-            step("Test for change on click") {
-                MainScreen {
-                    // button.click()
-                    toolbarTitle.hasText("Penn Mobile")
-                }
-            }
-        }
-    }
-}
+//import androidx.test.rule.ActivityTestRule
+//import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+//import org.junit.Rule
+//import org.junit.Test
+//
+//// Instrumentation tests, written using Kaspresso https://github.com/KasperskyLab/Kaspresso
+//class MainActivityTest : TestCase() {
+//
+//    @get:Rule
+//    val activityTestRule = ActivityTestRule(MainActivity::class.java, true, false)
+//
+//    @Test
+//    fun testMainActivity() {
+//        before {
+//            activityTestRule.launchActivity(null)
+//            testLogger.i("Starting test")
+//        }.after {
+//        }.run {
+//            step("Test setup") {
+//                MainScreen {
+//                    toolbarTitle.hasText("Penn Mobile")
+//                }
+//            }
+//            step("Test for change on click") {
+//                MainScreen {
+//                    // button.click()
+//                    toolbarTitle.hasText("Penn Mobile")
+//                }
+//            }
+//        }
+//    }
+//}
--- PennMobile/src/androidTest/java/MainScreen.kt
+++ PennMobile/src/androidTest/java/MainScreen.kt
@@ -1,11 +1,11 @@
 package com.pennapps.labs.pennmobile
 
-import com.agoda.kakao.text.KTextView
-import com.kaspersky.kaspresso.screens.KScreen
-
-object MainScreen : KScreen<MainScreen>() {
-    override val layoutId: Int? = R.layout.activity_main
-    override val viewClass: Class<*>? = MainActivity::class.java
-
-    val toolbarTitle = KTextView { withId(R.id.toolbar_title) }
-}
+//import com.agoda.kakao.text.KTextView
+//import com.kaspersky.kaspresso.screens.KScreen
+//
+//object MainScreen : KScreen<MainScreen>() {
+//    override val layoutId: Int? = R.layout.activity_main
+//    override val viewClass: Class<*>? = MainActivity::class.java
+//
+//    val toolbarTitle = KTextView { withId(R.id.toolbar_title) }
+//}
