fa2c155eda37d64a0f74bf815eb1ca49d3364510
bel11 <67809754+bel11@users.noreply.github.com>
2021-09-26 13:58:10 -0400

    added belinda to about page

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/adapters/AboutAdapter.kt
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/adapters/AboutAdapter.kt
@@ -37,6 +37,7 @@ class AboutAdapter(private var members: ArrayList<String>)
             "Liz Powell" -> R.drawable.liz
             "Anna Jiang" -> R.drawable.anna_jiang
             "Rohan Chhaya" -> R.drawable.rohan
+            "Belinda Xi" -> R.drawable.belinda
             else -> null
         }
         if (android.os.Build.VERSION.SDK_INT >=  android.os.Build.VERSION_CODES.M){
