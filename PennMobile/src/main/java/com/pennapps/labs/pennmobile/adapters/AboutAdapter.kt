de1b4cada166a1b8fd61d6a5de2f25315b9f71b9
rohan <rchhaya@seas.upenn.edu>
2022-10-01 00:07:52 -0400

    Updating alumni + adding Jenny

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/adapters/AboutAdapter.kt
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/adapters/AboutAdapter.kt
@@ -42,6 +42,7 @@ class AboutAdapter(private var members: ArrayList<String>)
             "Ansh Nagwekar" -> R.drawable.ansh
             "Zhiyan Lu" -> R.drawable.zhiyan
             "Ali Krema" -> R.drawable.ali
+            "Jenny Li" -> R.drawable.jenny
             else -> null
         }
         if (android.os.Build.VERSION.SDK_INT >=  android.os.Build.VERSION_CODES.M){
