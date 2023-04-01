a564232c777c13589323528dd5d5e0f21978dea3
Anthony Li <antli@wharton.upenn.edu>
2023-03-31 22:12:15 -0400

    Remove unused import

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/MainActivity.kt
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.pennapps.labs.pennmobile
 
-import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.Context
 import android.content.SharedPreferences
