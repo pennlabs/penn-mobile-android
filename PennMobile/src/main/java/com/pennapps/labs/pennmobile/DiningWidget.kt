40e12293c1bd03d2d7e14b72ce8a0b4cc2d37b11
vavali08 <vavali@seas.upenn.edu>
2023-11-05 13:45:55 -0500

    xml setup

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/DiningWidget.kt
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/DiningWidget.kt
@@ -26,10 +26,8 @@ class DiningWidget : AppWidgetProvider() {
 }
 
 internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
-    val widgetText = context.getString(R.string.appwidget_text)
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.dining_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
