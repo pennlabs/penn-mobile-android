5ed178c054841b497d7bcbfb0ecdbfc2dbb35a84
vavali08 <vavali@seas.upenn.edu>
2024-01-13 23:17:27 -0500

    nav updates etc

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/SubletterPostedListingsFragment.kt
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/SubletterPostedListingsFragment.kt
@@ -5,8 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.pennapps.labs.pennmobile.databinding.FragmentSubletterPostedListingsBinding
 
 class SubletterPostedListingsFragment : Fragment() {
+    private var _binding: FragmentSubletterPostedListingsBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -14,6 +18,23 @@ class SubletterPostedListingsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_subletter_posted_listings, container, false)
+        _binding = FragmentSubletterPostedListingsBinding.inflate(inflater, container, false)
+        binding.postedAddListingButton.setOnClickListener{
+            navigateCreateNewListing()
+        }
+        return binding.root
+    }
+
+
+    private fun navigateCreateNewListing() {
+        val mainActivity = activity as MainActivity
+        val fragment = NewListingsFragment()
+
+        val fragmentManager = mainActivity.supportFragmentManager
+        fragmentManager.beginTransaction()
+                .replace(R.id.content_frame, fragment, "NEW_LISTING_FRAGMENT")
+                .addToBackStack(null)
+                .commitAllowingStateLoss()
+
     }
 }
