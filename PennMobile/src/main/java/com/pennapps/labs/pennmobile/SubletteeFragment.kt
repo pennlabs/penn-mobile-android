a9c51047682125cde5cf0fd5390e831754aac973
trinif <trinilaurenf@gmail.com>
2024-01-19 12:27:21 -0500

    basic views for intro sublettee page and marketplace, cardview and adapter for sublettee entry

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/SubletteeFragment.kt
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/SubletteeFragment.kt
@@ -5,6 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.FragmentTransaction
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.pennapps.labs.pennmobile.api.StudentLife
+import com.pennapps.labs.pennmobile.databinding.FragmentGsrBinding
+import com.pennapps.labs.pennmobile.databinding.FragmentSubletteeViewBinding
+import org.joda.time.DateTime
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,16 +26,49 @@ private const val ARG_PARAM2 = "param2"
 class SubletteeFragment : Fragment() {
 
 
+    //create binding
+    private var _binding : FragmentSubletteeViewBinding? = null
+    private val binding get() = _binding!!
+
+    //api manager
+    private lateinit var mStudentLife: StudentLife
+
+    private lateinit var mActivity: MainActivity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mStudentLife = MainActivity.studentLifeInstance
+        mActivity = activity as MainActivity
+        mActivity.closeKeyboard()
+
+        val bundle = Bundle()
+
+        //edit this later for firebase
+        /* bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "0")
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "GSR")
+        bundle.putString(FirebaseAnalytics.Param.ITEM_CATEGORY, "App Feature")
+        FirebaseAnalytics.getInstance(mActivity).logEvent(FirebaseAnalytics.Event.VIEW_ITEM, bundle) */
 
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sublettee_view, container, false)
+        _binding = FragmentSubletteeViewBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        val marketplaceButton: Button = view.findViewById(R.id.sublettee_enter_subletting_button)
+
+        marketplaceButton.setOnClickListener {
+            //load new fragment, which will hold the subletting marketplace in whole
+            mActivity.supportFragmentManager.beginTransaction()
+                    .replace(((view as ViewGroup).parent as View).id, SubletteeMarketplace())
+                    .addToBackStack(null)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+        }
     }
 
 }
---
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/adapters/SublettingListAdapter.kt
@@ -0,0 +1,49 @@
+package com.pennapps.labs.pennmobile.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.pennapps.labs.pennmobile.R
+import com.pennapps.labs.pennmobile.classes.SublettingModel
+
+class SublettingListAdapter(var ctx: android.content.Context?, var sublettingList: ArrayList<SublettingModel>):
+        RecyclerView.Adapter<SublettingListAdapter.SublettingCardViewHolder>() {
+
+    class SublettingCardViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        var listingImage = itemView.findViewById<ImageView>(R.id.subletting_cardview_image)
+        var listingTitle = itemView.findViewById<TextView>(R.id.subletting_cardview_title)
+        var listingPrice = itemView.findViewById<TextView>(R.id.subletting_cardview_price)
+        var listingRooms = itemView.findViewById<TextView>(R.id.subletting_cardview_rooms)
+        var listingDates = itemView.findViewById<TextView>(R.id.subletting_cardview_dates)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SublettingCardViewHolder {
+        val newView = LayoutInflater.from(parent.context).inflate(R.layout.subletting_cardview, parent, false)
+        return SublettingCardViewHolder(newView)
+    }
+
+    override fun onBindViewHolder(holder: SublettingCardViewHolder, position: Int) {
+        var mSublettingCard: SublettingModel = sublettingList[position]
+        holder.listingImage.setImageResource(mSublettingCard.listingImage!!)
+        holder.listingTitle.text = mSublettingCard.listingTitle
+
+        //price, adding negotiable if price is negotiable
+        var price = "$" + mSublettingCard.listingPrice.toString()
+        if (mSublettingCard.isNegotiable == true) {
+            price += " (negotiable)"
+        }
+        holder.listingPrice.text = price
+
+        val rooms = mSublettingCard.numberBeds.toString() + " bd | " +
+                mSublettingCard.numberBath.toString() + " ba"
+        holder.listingRooms.text = rooms
+    }
+
+    override fun getItemCount(): Int {
+        return sublettingList.size
+    }
+}
