b4719c8e63b4427374324fe8ce4bb76f10fd5896
meiron03 <meiaaron99@gmail.com>
2024-03-20 12:16:59 -0400

    Reduce jank by notifying updated positions instead of resetting the adapter.

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/LaundryFragment.kt
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/LaundryFragment.kt
@@ -38,13 +38,10 @@ class LaundryFragment : Fragment() {
     private var laundryRooms = ArrayList<LaundryRoom>()
     // data for laundry room usage
     private var roomsData: ArrayList<LaundryUsage> = ArrayList()
-
-    private var laundryRoomsResult = ArrayList<LaundryRoom>()
-    private var roomsDataResult: MutableList<LaundryUsage> = ArrayList()
+    private val maxRooms = 3
 
     private var mAdapter: LaundryRoomAdapter? = null
 
-    private var count: Int = 0
     private var numRooms: Int = 0
 
     private var _binding : FragmentLaundryBinding? = null
@@ -65,29 +62,11 @@ class LaundryFragment : Fragment() {
 
         initAppBar()
 
-        // get num rooms to display
-        sp = PreferenceManager.getDefaultSharedPreferences(mContext)
-        numRooms = sp?.getInt(mContext.getString(R.string.num_rooms_pref), 100) ?: 0
-        count = 0
-        for (i in 0 until numRooms) {
-            if (sp!!.getBoolean(i.toString(), false)) {
-                count += 1
-            }
-        }
-
         binding.favoriteLaundryList.layoutManager = LinearLayoutManager(mContext)
         binding.laundryMachineRefresh.setOnRefreshListener {
-            laundryViewModel.getFavorites(mStudentLife, "Bearer frEnhz2PW8VkIUA4XX0sUy61N6Yc41")
+            laundryViewModel.getFavorites(mStudentLife, "Bearer eEc79lTYqHBuUHssUzTYXiImyMG6U9")
         }
         binding.laundryMachineRefresh.setColorSchemeResources(R.color.color_accent, R.color.color_primary)
-
-        // no rooms chosen
-        if (count == 0) {
-            view.loadingPanel?.visibility = View.GONE
-            binding.laundryHelpText.visibility = View.VISIBLE
-            mAdapter = LaundryRoomAdapter(mContext, laundryRooms, roomsData, false)
-            binding.favoriteLaundryList.adapter = mAdapter
-        }
 
         return view
     }
@@ -102,15 +81,11 @@ class LaundryFragment : Fragment() {
         mActivity.removeTabs()
         numRooms = sp?.getInt(mContext.getString(R.string.num_rooms_pref), 100) ?: 0
 
-        // get num rooms to display
-        count = 0
-        for (i in 0 until numRooms) {
-            if (sp?.getBoolean(i.toString(), false) == true) {
-                count += 1
-            }
-        }
         mActivity.setTitle(R.string.laundry)
         loadingPanel?.visibility = View.VISIBLE
+
+        mAdapter = LaundryRoomAdapter(mContext, laundryRooms, roomsData, false)
+        binding.favoriteLaundryList.adapter = mAdapter
 
         laundryViewModel.favoriteRooms.observe(viewLifecycleOwner) { favorites ->
             binding.laundryMachineRefresh.isRefreshing = false
@@ -121,14 +96,15 @@ class LaundryFragment : Fragment() {
             laundryRooms.addAll(favorites.favoriteRooms)
             roomsData.addAll(favorites.roomsData)
 
-            mAdapter = LaundryRoomAdapter(mContext, laundryRooms, roomsData, false)
-            binding.favoriteLaundryList.adapter = mAdapter
+            for (pos in 0 until maxRooms) {
+                mAdapter!!.notifyItemChanged(pos)
+            }
 
             loadingPanel?.visibility = View.GONE
             binding.laundryHelpText.visibility = View.INVISIBLE
         }
 
-        laundryViewModel.getFavorites(mStudentLife, "Bearer frEnhz2PW8VkIUA4XX0sUy61N6Yc41")
+        laundryViewModel.getFavorites(mStudentLife, "Bearer eEc79lTYqHBuUHssUzTYXiImyMG6U9")
     }
 
     private fun initAppBar() {
@@ -144,123 +120,4 @@ class LaundryFragment : Fragment() {
                     .commit()
         }
     }
-
-    private fun updateRooms() {
-        //displays banner if not connected
-        if (!isOnline(context)) {
-            binding.internetConnectionLaundry.setBackgroundColor(resources.getColor(R.color.darkRedBackground))
-            binding.internetConnectionMessageLaundry.text = getString(R.string.internet_error)
-            binding.internetConnectionLaundry.visibility = View.VISIBLE
-        } else {
-            binding.internetConnectionLaundry.visibility = View.GONE
-        }
-
-        laundryRooms = ArrayList()
-        roomsData = ArrayList()
-        roomsDataResult = ArrayList()
-        laundryRoomsResult = ArrayList()
-
-        count = 0
-        for (i in 0 until numRooms) {
-            if (sp?.getBoolean(i.toString(), false) == true) {
-                count += 1
-            }
-        }
-
-        // add data
-        for (i in 0 until numRooms) {
-            if (sp!!.getBoolean(i.toString(), false)) {
-                addAvailability(i)
-                addRoom(i)
-            }
-        }
-
-        // no rooms chosen
-        if (count == 0) {
-            loadingPanel?.visibility = View.GONE
-            binding.laundryHelpText.visibility = View.VISIBLE
-            mAdapter = LaundryRoomAdapter(mContext, laundryRooms, roomsData, false)
-            binding.favoriteLaundryList.adapter = mAdapter
-        }
-    }
-
-    @Synchronized
-    private fun addRoomToList(room: LaundryRoom) {
-        laundryRoomsResult.add(room)
-    }
-
-    @Synchronized
-    private fun addUsageToList(usage: LaundryUsage) {
-        roomsDataResult.add(usage)
-    }
-
-    private fun addRoom(i: Int) {
-        mStudentLife.room(i)?.subscribe({ room ->
-                    room.id = i
-                    addRoomToList(room)
-
-                    if (laundryRoomsResult.size == count) {
-
-                        // sort laundry rooms data by hall name
-                        roomsDataResult.sortWith { usage1, usage2 -> usage2.id - usage1.id }
-
-                        // sort laundry rooms by name
-                        laundryRoomsResult.sortWith { room1, room2 -> room2.id - room1.id }
-
-                        // update UI
-                        mActivity.runOnUiThread {
-                            roomsData = roomsDataResult as ArrayList<LaundryUsage>
-                            laundryRooms = laundryRoomsResult
-                            mAdapter = LaundryRoomAdapter(mContext, laundryRooms, roomsData, false)
-                            try {
-                                binding.favoriteLaundryList.adapter = mAdapter
-                                binding.laundryHelpText.visibility = View.INVISIBLE
-                                binding.laundryMachineRefresh.isRefreshing = false
-                            } catch (e: Exception) {
-                                FirebaseCrashlytics.getInstance().recordException(e)
-                            }
-                            no_results?.visibility = View.GONE
-                            loadingPanel?.visibility = View.GONE
-                        }
-                    }
-                }, {
-                    mActivity.runOnUiThread {
-                        try {
-                            binding.laundryHelpText.visibility = View.GONE
-                            binding.laundryMachineRefresh.isRefreshing = false
-                        } catch (e: Exception) {
-                            FirebaseCrashlytics.getInstance().recordException(e)
-                        }
-                        loadingPanel?.visibility = View.GONE
-                        no_results?.visibility = View.VISIBLE
-                        Log.e("Laundry", "Error getting laundry data: " + it.stackTrace)
-                    }
-                })
-    }
-
-    private fun addAvailability(i: Int) {
-        mStudentLife.usage(i)?.subscribe({ usage ->
-                    usage.id = i
-                    addUsageToList(usage)
-                }, {
-                    // in case usage data not available - set chart to 0
-                    val newUsage = LaundryUsage()
-                    newUsage.setWasherData()
-                    newUsage.setDryerData()
-                    newUsage.id = i
-                    roomsDataResult.add(newUsage)
-
-                    mActivity.runOnUiThread {
-                        loadingPanel?.visibility = View.GONE
-                        no_results?.visibility = View.VISIBLE
-                        try {
-                            binding.laundryHelpText.visibility = View.GONE
-                            binding.laundryMachineRefresh.isRefreshing = false
-                        } catch (e: Exception) {
-                            FirebaseCrashlytics.getInstance().recordException(e)
-                        }
-                    }
-                })
-    }
-
 }
