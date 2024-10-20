6b1fe8ebf8dde008c0ec19778d40312beecff3b8
Divak2004 <fu_david@outlook.com>
2024-10-20 11:50:56 -0400

    Gsr widget now clickable

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/adapters/GsrReservationWidgetAdapter.kt
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/adapters/GsrReservationWidgetAdapter.kt
@@ -1,6 +1,5 @@
 package com.pennapps.labs.pennmobile.adapters
 
-import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.content.Context
 import android.content.Intent
