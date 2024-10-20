188ce8e1f7c7174cee15299ebe2e4b1eb2d8d491
Divak2004 <fu_david@outlook.com>
2024-10-20 02:44:35 -0400

    GSR Reservation widget mostly done

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/api/GsrReservationsRequest.java
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/api/GsrReservationsRequest.java
@@ -1,2 +1,22 @@
-package com.pennapps.labs.pennmobile.api;public interface GsrReservationsRequest {
+package com.pennapps.labs.pennmobile.api;
+
+import com.pennapps.labs.pennmobile.classes.AccessTokenResponse;
+import com.pennapps.labs.pennmobile.classes.GSRReservation;
+import com.pennapps.labs.pennmobile.classes.Venue;
+
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
+import retrofit.http.GET;
+import retrofit.http.Header;
+import retrofit.http.POST;
+import rx.Observable;
+
+public interface GsrReservationsRequest {
+    @GET("/gsr/reservations")
+    Observable<List<GSRReservation>> getGsrReservations(
+            @Header("Authorization") String bearerToken
+    );
 }
