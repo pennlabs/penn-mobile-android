19c257042408b96bab28ea8201844b915a8e57a0
David Fu <fu_david@outlook.com>
2025-02-23 14:32:31 -0500

    widget should update every 30 mins

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/gsr/widget/AppWidgetAlarm.kt
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/gsr/widget/AppWidgetAlarm.kt
@@ -4,6 +4,7 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import com.pennapps.labs.pennmobile.gsr.widget.GsrReservationWidget
 import java.util.Calendar
 
 // An AlarmManager for app widget testing (Shortens the update time)
@@ -16,8 +17,8 @@ class AppWidgetAlarm(
     fun startAlarm() {
         val calendar: Calendar = Calendar.getInstance()
         calendar.add(Calendar.MILLISECOND, internalMillis)
-        val alarmIntent = Intent(mContext, DiningHallWidget::class.java)
-        alarmIntent.setAction(DiningHallWidget.ACTION_AUTO_UPDATE)
+        val alarmIntent = Intent(mContext, GsrReservationWidget::class.java)
+        alarmIntent.setAction(GsrReservationWidget.ACTION_AUTO_UPDATE)
         val pendingIntent = PendingIntent.getBroadcast(mContext, alarmId, alarmIntent, PendingIntent.FLAG_IMMUTABLE)
         val alarmManager: AlarmManager = mContext.getSystemService(Context.ALARM_SERVICE) as AlarmManager
         alarmManager.setInexactRepeating(
@@ -29,18 +30,15 @@ class AppWidgetAlarm(
     }
 
     fun stopAlarm() {
-        val alarmIntent = Intent(DiningHallWidget.ACTION_AUTO_UPDATE)
+        val alarmIntent = Intent(GsrReservationWidget.ACTION_AUTO_UPDATE)
         val pendingIntent = PendingIntent.getBroadcast(mContext, alarmId, alarmIntent, PendingIntent.FLAG_IMMUTABLE)
         val alarmManager: AlarmManager = mContext.getSystemService(Context.ALARM_SERVICE) as AlarmManager
         alarmManager.cancel(pendingIntent)
     }
 
     fun alarmUp(): Boolean {
-        val alarmIntent = Intent(DiningHallWidget.ACTION_AUTO_UPDATE)
+        val alarmIntent = Intent(GsrReservationWidget.ACTION_AUTO_UPDATE)
         val pendingIntent = PendingIntent.getBroadcast(mContext, alarmId, alarmIntent, PendingIntent.FLAG_IMMUTABLE)
-        if (pendingIntent != null) {
-            return true
-        }
-        return false
+        return pendingIntent != null
     }
 }
