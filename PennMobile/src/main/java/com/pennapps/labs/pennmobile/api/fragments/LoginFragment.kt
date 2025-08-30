4b1154cd29dd16e11078324d39aad45a2f41ec4e
Skeletrobro <67814129+baronhsieh2005@users.noreply.github.com>
2025-08-30 00:29:09 -0400

    Small adjustments

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/api/fragments/LoginFragment.kt
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/api/fragments/LoginFragment.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import androidx.preference.PreferenceManager
