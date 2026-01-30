5544bd0f3ef1262dbeb7fd38f654ab8c03a3e6f1
David Fu <fu_david@outlook.com>
2026-01-30 18:43:19 -0500

    Change start day of semester

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/dining/adapters/DiningInsightsCardAdapter.kt
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/dining/adapters/DiningInsightsCardAdapter.kt
@@ -52,7 +52,7 @@ class DiningInsightsCardAdapter(
         private const val DINING_DOLLARS_PREDICTIONS = 2
         private const val DINING_SWIPES_PREDICTIONS = 3
 
-        const val START_DAY_OF_SEMESTER = "2025-01-15"
+        const val START_DAY_OF_SEMESTER = "2026-01-14"
         const val DAYS_IN_SEMESTER = 119f
     }
 
